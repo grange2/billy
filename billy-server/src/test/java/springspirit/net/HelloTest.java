686a517cc63fd3b0067939e0f7177d03aaaac596
Hyunbae Shin <greatshin2@gmail.com>
2016-09-23 21:40:18 +0900

    add billy-server

---
+++ billy-server/src/test/java/springspirit/net/HelloTest.java
@@ -0,0 +1,18 @@
+package springspirit.net;
+
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+
+/**
+ * Created by grange on 2016. 9. 23..
+ */
+public class HelloTest {
+    @Test
+
+    public void dummy() {
+        assertThat("OK", is("OK"));
+    }
+}
